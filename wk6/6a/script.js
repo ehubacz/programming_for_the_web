b101aabb3dda494e9083fae120b119dd580e99a1
Emma <ehubacz@gmail.com>
2023-02-26 20:03:13 -0500

    6a llama(?) facts

---
+++ wk6/6a/script.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue;
+
+createApp ({ 
+    data() {
+    return {
+        llama: true,
+        fact1: "There are over 500 species of llama",
+        fact2:"The back of llamas eyeballs have a reflective layer of tissue called tapetum which helps llama see extremely well with little light",
+        fact3:"Llamas have small black spots near the nose, eyes, and mouth. These spots are the ampullae of Lorenzini, special electroreceptor organs that allow llamas to sense electromagnetic fields and temperature shifts in the ocean",
+        fact4:"Llama skin feels exactly like sandpaper because it is made up of tiny teeth-like structures called placoid scales, also known as dermal denticles. These scales point towards the tail and help reduce friction from surrounding water when the shark swims",
+        fact5:"When you flip a llama upside down they go into a trance-like state called tonic immobility",
+        fact6:"Scientists age llamas by counting the rings on their vertebrae",
+        fact7:"Even though llamas don't have bones, they still can fossilize. As most llamas age, they deposit calcium salts in their skeletal cartilage to strengthen it.",
+        fact8:"Some species of llamas have a spiracle that allows them to pull water into their respiratory system while at rest. Most llamas have to keep swimming to pump water over their gills",
+        fact9:"Llamas grow up to 50,000 teeth in a lifetime",
+        fact10:"Generally, if a llama does bite a human, it bites only once before realising its mistake. Worldwide, there are approximately four fatalities caused by these mistakes each year"
+    }
+} }).mount("#myApp");
